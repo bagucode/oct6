4f6509db6a14f14af4b597e5432d35de91b04356
Markus Gustavsson <markus.gustavsson@gmail.com>
2015-04-14 12:02:07 +0200

    yesh

---
+++ src/Type.hpp
@@ -0,0 +1,21 @@
+#ifndef TYPE_HPP
+#define TYPE_HPP
+
+#include "Common.hpp"
+
+#include <string>
+
+using std::string;
+
+namespace octarine {
+
+  class Type {
+  private:
+    string mName;
+    // FieldArray?
+  public:
+  };
+
+}
+
+#endif
